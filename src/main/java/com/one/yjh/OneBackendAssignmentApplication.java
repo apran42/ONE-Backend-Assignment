dc61ac518aeb0a282421ebd4c4670e54751dc764
apran42 <flyingdisc1@naver.com>
2026-04-30 13:57:31 +0900

    init: 새로운 프로젝트 생성

---
+++ src/main/java/com/one/yjh/OneBackendAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.one.yjh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneBackendAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneBackendAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/one/yjh/OneBackendAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.one.yjh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneBackendAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
