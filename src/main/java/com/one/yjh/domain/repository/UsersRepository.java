753790845a5abcf01134c8aa8d0ad2b44cac3e13
apran42 <flyingdisc1@naver.com>
2026-05-06 13:40:16 +0900

    fix: 에러코드 수정

--- src/main/java/com/one/yjh/domain/repository/UsersRepository.java
+++ src/main/java/com/one/yjh/domain/repository/UsersRepository.java
@@ -1,6 +1,7 @@
 package com.one.yjh.domain.repository;
 
 import com.one.yjh.domain.entity.Users;
+import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,5 @@ import java.util.Optional;
 @Repository
 public interface UsersRepository extends JpaRepository<Users, Long>{
     Optional<Users> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
