64fcfc1659c186a9c2a550e471d0dee739f2a2aa
apran42 <flyingdisc1@naver.com>
2026-05-07 02:43:40 +0900

    fix: 에러 코드 추가

--- src/main/java/com/one/yjh/global/exception/ErrorCode.java
+++ src/main/java/com/one/yjh/global/exception/ErrorCode.java
@@ -15,8 +15,9 @@ public enum ErrorCode {
     // 토큰
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "A001", "유효하지 않은 토큰입니다."),
     EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "A002", "만료된 토큰입니다."),
-    TOKEN_NOT_FOUND(HttpStatus.UNAUTHORIZED, "A003", "인증 토큰이 누락되었습니다.");
-
+    TOKEN_NOT_FOUND(HttpStatus.UNAUTHORIZED, "A003", "인증 토큰이 누락되었습니다."),
+    UNSUPPORTED_TOKEN(HttpStatus.UNAUTHORIZED, "A004", "지원하지 않는 형식의 토큰입니다."),
+    EMPTY_TOKEN(HttpStatus.UNAUTHORIZED, "A005", "토큰이 비어있습니다.");
     private final HttpStatus status;
     private final String code;
     private final String message;
